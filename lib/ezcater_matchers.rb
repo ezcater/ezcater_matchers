4b232bde0792989583ca6e0d5664a285386d98bd
Winfield Peterson <winfield.peterson@gmail.com>
2018-06-07 09:02:39 -0400

    Include match_models

--- lib/ezcater_matchers.rb
+++ lib/ezcater_matchers.rb
@@ -2,5 +2,6 @@ require "rspec"
 
 require "ezcater_matchers/have_been_destroyed"
 require "ezcater_matchers/match_db_timestamp"
+require "ezcater_matchers/match_models"
 require "ezcater_matchers/match_ordered_array"
 require "ezcater_matchers/version"
