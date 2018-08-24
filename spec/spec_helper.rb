f6b996555d61c7b6251346bd0a1ed1fd1e80e87e
Tim Perkins <tim.perkins@ezcater.com>
2018-08-24 18:44:40 -0400

    Report test coverage to Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,9 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "simplecov"
-SimpleCov.start
+SimpleCov.start do
+  add_filter("/spec/")
+end
 
 require "ezcater_matchers"
 require "ezcater_gem/rspec"
